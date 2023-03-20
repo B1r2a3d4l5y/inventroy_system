d34aa81c2f27265b909daccf7ad0e471f82f9ecd
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2023-03-20 14:41:21 +0200

    Add files via upload

--- includes/dashboard.php
+++ includes/dashboard.php
@@ -20,50 +20,53 @@ if(isset($_SESSION['user'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
-    
-
-    <title>Dashboard</title>
+    <link rel="stylesheet" href="CSS/dashbord.css">
     <style>
-        :before::after {
-    margin: 0;
+        ::before::after {
+    margin 0;
     padding: 0;
-    box-sizing: border-box;
-}
-.logo-box {
-    background-color : black;
-    width: 100%;
-    border: none;
-    border-radius : 5px;
-
-
-
-
-}
-.logo {
-    display:inline-block ;
-    color: red;
-    font-size : 70px;
-    font-weight : bold;
-
-}
-.logo:hover {
-    color: red
-
-}
-.menu {
-    background-color : blue;
-    width : 100%;
-    
 }
 
+ .logo-box {
+     background-color: black;
+     width: 100%;
+     border: none;
+     border-radius: 5px;
 
 
-.admin {
 
-}
+
+ }
+
+ .logo {
+     display: inline-block;
+     color: red;
+     font-size: 70px;
+     font-weight: bold;
+
+ }
+
+ .logo:hover {
+     color: red
+ }
+
+ .menu {
+     background-color: blue;
+     width: 100%;
+
+ }
+
+
+
+ .admin {
+    font-size: 35px;
+    font-weight:bold;
+ }
 
     </style>
+
+    <title>Dashboard</title>
+      
 </head>
 <body>
     <div class="container">
@@ -75,62 +78,62 @@ if(isset($_SESSION['user'])) {
                 <a class="dashboard" href="includes/dashbord.php">Dashboard</a>
                 <a class="products" href="products.php" >Products</a>
                 <a class="users" href="users.php">Users</a>
+                      <p class="admin"><?php echo $_SESSION['user']?></p>
                  <a class="logout btn btn-danger btn-md " href="includes/logout.php">Logout </a>
-                <p class="admin"><?php echo $_SESSION['user']?></p>
+          
             </nav>
             
 
             </div>
-           <form class="form col-lg-6 col-md-6 col-sm-12" action="" method="GET">
-             <table class="table">
+          
+             <table class="table highest_selling">
                 <thead>
                     <tr>
                         <th>Highest selling products</th>
 
 
-                        <tr>
                             <td >Title</td>
                             <td >Total Sold</td>
-                            <td >Total Quanity</td>
+                            <td >Total Quanity</td>   
 
+                    </tr>
                 </thead>
                 <tbody>
                     <?php
                     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+                    ini_set('memory_limit', -1);
                     $serverHost = "localhost";
                     $Username = "root";
                     $dbPassword = "";
                     $dbName = "products";
                     $conn =  new mysqli($serverHost, $Username, $dbPassword, $dbName);
-                    $conn->query("DROP TABLE IF EXISTS highest_selling");
+                    $conn->query("DROP TABLE IF EXISTS  highest_selling;");
                     $conn->query("CREATE TABLE highest_selling(
                         title VARCHAR(30) NOT NULL,
                         total_sold INTEGER NOT NULL,
                         total_quanity INTEGER NOT NULL
                         
                     );");
-                    $conn->query("INSERT INTO highest_selling(title, total_sold, total_quanity) VALUES('buble wrap', 2 , 4 )");
-                    $conn->query("INSERT INTO highest_selling(title, total_sold, total_quanity) VALUES('USB cable' , 2 , 12)");
-                    $conn->query("INSERT INTO highest_selling(title, total_sold, total_quanity) VALUES('Lord of the rings board game' , 2 , 4) ");
-                    $stmt = $conn->prepare ("SELECT * FROM highest_selling");
+                  $conn->query("INSERT INTO  highest_selling(title, total_sold, total_quanity) VALUES('buble wrap', 2, 3)");
+                  $conn->query("INSERT INTO highest_selling(title, total_sold, total_quanity) VALUES('USB cable', 3, 2 )");
+                  $conn->query("INSERT INTO highest_selling(title, total_sold, total_quanity) VALUES('Lord Of the Rings board game', 3, 1) ");
+
+                    $stmt = $conn->prepare ("SELECT * FROM highest_selling LIMIT 5");
                     $stmt->execute();
                     $result = $stmt->get_result();
                     $row = $result->fetch_array();
-                    while($row) {
+                    while($row){
                         echo "<tr>";
-                        echo "<td>. $row[title] . </td>";
-                        echo "<td>.$row[total_sold] . </td>";
-                        echo "<td>. $row[total_quanity]. </td>";
+                        echo "<td>. $row[title] . </td> ";
+                        echo "<td>.$row[total_sold] . </td> ";
+                        echo "<td>.  $row[total_quanity]. </td> ";
                         echo "</tr>";
-                        
-
                     }
 
 
                     ?>
                 </tbody>
             </table>
-           </form>
         </div>
     </div>
     

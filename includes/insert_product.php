8cf668aaa9fb87e239cc8a3bd782585ee7ed49a5
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2023-03-30 12:52:30 +0200

    Add files via upload

--- includes/insert_product.php
+++ includes/insert_product.php
@@ -1,11 +1,30 @@
 <?php
-require "connection.php";
+$serverHost = "localhost";
+$dbName = "products";
+$username = "root";
+$password = "";
 if(isset($_POST['add'])) {
     $product_name = trim($_POST["product_name"]);
-    $quanity =  trim($_POST["quanity"])
-    $product_price trim($_POST["product_price"]);
+    $quanity =  trim($_POST["quanity"]);
+    $product_price =  trim($_POST["productprice"]);
 
+    $db = new PDO("mysql:host=$serverHost; dbname=$dbName", $username, $password);
+    if(empty($product_name) || empty($quanity) || empty($product_price)) {
+        header("Location:products.php?fields=empty");
+        exit;
+
+    } else {
+        $insert = $db->prepare("INSERT INTO products(product_name , quanity, price) VALUES(:product_price, :quanity, :price) ") ;
+        $insert->bind_param(":product_name",$product_name );
+        $insert->bind_param(":quanity", $quanity);
+        $insert->bind_param(":price", $product_price);
+        $insert->execute();
+        $insert->close();
+        
+
+    }
 
 
 }
+$db = null;
 ?>

5442dbddc782fa0af34220c68737448beb34ab73
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2023-04-11 14:32:07 +0200

    Add files via upload

--- includes/log_user.php
+++ includes/log_user.php
@@ -9,31 +9,38 @@ $dbpassword = "";
 
 
 if(isset($_POST["login"])) {
-    $username = trim($_POST["user"]);
-    $password = trim($_POST["password"]);
-    $pass_hash = password_hash($password , PASSWORD_DEFAULT);
+    $user = trim($_POST["user"]);
+    $password = $_POST["password"];
+    $password_hash = password_hash($password , PASSWORD_DEFAULT);
 
-    if(empty($username) || empty($password)) {
+    if(empty($user) || empty($password)) {
         header("Location:../login.php?Fields=empty");
         exit;
     } else {
-        
-        $db = new PDO("mysql:host=$serverHost; dbname=$dbname", $dbusername, $dbpassword );
-
-       $stmt = $db->prepare("SELECT * FROM  users WHERE username=$username AND password=$password ");
-        $stmt->bindParam(":username", $username);
-        $stmt->bindParam(":password", $password);
-        $stmt->execute();
-        
-        while($stmt->fetch(PDO::FETCH_ASSOC)) {
-            session_start();
-            $_SESSION["logged"] = 1;
-            $_SESSION["user"] = $username;
-            $_SESSION["password"] = $password;
-            header("Location:dashboard.php");
-            exit;
+        // check database connection
+        try {
+              $db = new PDO("mysql:host=$serverHost; $dbname=$dbname", $dbusername, $dbpassword);
+              $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              echo "connection successful";
+        } catch(PDOEception $exception)  {
+            echo "connnection failed:".$exception->getMessage();
         }
+      
     }
-}
-$db = null;
-?>
+    $stmt = $db->prepare("SELECT *  FROM users WHERE $username=$user AND password=$password ");
+    $stmt->bindParam(":username", $user, PDO::PARAM_STR);
+    $stmt->bindParam(":password", $password, PDO::PARAM_STR);
+    $stmt->execute();
+    
+    while($stmt->fetch(PDO::FETCH_ASSOC)) {
+        session_start();
+        $_SESSION["logged"] = 1;
+        $_SESSION["user"] = $user;
+        $_SESSION["password"] = $password;
+        header("Location:dashboard.php");
+        exit;
+    }
+   
+
+   }
+   $db = null;

0d65da0cc036b2a5f5865994d162d3386a49aaa3
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2023-04-13 13:51:50 +0200

    Add files via upload

--- includes/log_user.php
+++ includes/log_user.php
@@ -1,39 +1,45 @@
 <?php
-include("connection.php");
+session_start();
+
+$serverHost = "localhost";
+$dbname = "login";
+$dbusername = "root";
+$dbpassword = "";
+
+ try {
+    $db = new PDO("mysql:host=$serverHost; dbname=$dbname", $dbusername, $dbpassword);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connection sucessful";
+ } catch(PDOException $exception) {
+    echo "connection failed".$excption->getMessage();
+
+
+
+ }
 
 if(isset($_POST["login"])) {
+    
     $user = trim($_POST["user"]);
-    $password = $_POST["password"];
-    $hash_password = password_hash($password , PASSWORD_DEFAULT);
+    $password = trim($_POST["password"]);
+    $hash_password = password_hash($password, PASSWORD_DEFAULT);
 
     if(empty($user) || empty($password)) {
-        header("Location:../login.php?Fields=empty");
+        header("Location:../login.php?fields=empty");
         exit;
     } else {
-        // check database connection and connect
-       
-      
+        $query = $db->query("SELECT * FROM users  WHERE  username=$user AND  password=$hash_password");
+        $result= $db->prepare($query);
+        $result->execute(array("username"=> $user , "password" =>$hash_password));
+        while($result->fetch(PDO::FETCH_ASSOC)) {
+            $_SESSION["logged"] = 1;
+            $_SESSION["user"] = $user;
+            header("Location: dashboard.php");
+            exit;
+        }
+
+
+     
     }
-     $stmt = $db->prepare("SELECT *  FROM  users WHERE :username=$user AND :password=$hash_password ");
-    $stmt->bindParam(":username" , $user);
-    $stmt->bindParam(":password", $password ) ;
-    var_dump($stmt);
-    
-    while($stmt->fetch(PDO::FETCH_ASSOC)) {
-        //start the session
-         session_start();
-        $_SESSION["logged"] = 1; // login session
-        $_SESSION["user"] = $user; // login by username
-        $_SESSION["password"] = $hash_password;
-        header("Location: dashboard.php");
-        exit;
-    }
-   
-   
-
-   }
-   //close connection 
-   $db = null;
-
-
+}
+$db = null;
 ?>

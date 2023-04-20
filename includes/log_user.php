81e26ba63f5329527383223fa8b3bbfb0c06c3c2
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2023-04-20 13:08:56 +0200

    Add files via upload

--- includes/log_user.php
+++ includes/log_user.php
@@ -8,45 +8,48 @@ $dbusername = "root";
 $dbpassword = "";
 
 try {
-  $db = new PDO("mysql:host=$serverHost; $dbname=$dbname", $dbusername, $dbpassword);
-  
+  $db = new PDO("mysql:host=$serverHost; dbname=$dbname", $dbusername, $dbpassword);
+
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  if(isset($_POST["login"]) ) {
+  echo "connection suceesful";
+  
+  } catch(PDOEXception $error) {
+
+ $message =  $error->getMessage();
+  }
+ 
+
+if(isset($_POST["login"]) ) {
 
     $user = trim($_POST["user"] ) ;
-    $password = trim($_POST[$password]);
+    $password = trim($_POST["password"]);
     $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
-    if(empty($user) || empty($password)) {
+    if(empty($user) || empty($password_hash)) {
       header("Location:../login.php?fields=empty");
       exit;
 
     }  else {
-      $query = "SELECT * FROM users WHERE :username = $user AND :password = $password_hash";
-      $statement = $db->prepare($query);
-      $statement->bindzParam(":username", $user, PDO::PARAM_STR);
-      $statement->bindParam(":password", $password,  PDO::PARAM_STR);
-      $statement->execute();
-      $num_rows = $statement->rowCount();
+      $statement = $db->prepare("SELECT * FROM users WHERE :username=$user AND  :password=$password_hash");
+   $statement->bindParam(":username", $user, PDO::PARAM_STR);
+   $statement->bindParam(":password" , $password_hash, PDO::PARAM_STR);
+   $statement->execute();
 
-      if($num_rows > 0) {
-        $_SESSION["logged"] = 1;
+      while ($statement->fetch(PDO::FETCH_ASSOC)) {
+         $_SESSION["logged"] = 1;
         $_SESSION["user"] = $user;
         $_SESSION["password"] = $password_hash;
         header("Location:dashboard.php");
         exit;
-      } else {
-        header("../login.php?login=failed");
-        exit;
       }
+       
+      
+      
 
     }
+  
+
+
+
   }
-} catch(PDOEXception $error) {
-
- $message =  $error->getMessage();
-}
-
-$db = null;
-
-
+  $db = null;

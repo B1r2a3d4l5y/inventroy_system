a26503e5dc5eab1f6d74f46da1fd59a7943a02b5
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2023-04-26 09:37:33 +0200

    Add files via upload

--- includes/users.php
+++ includes/users.php
@@ -7,19 +7,41 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-        <link rel="stylesheet" href="./CSS/users.css" >
 
     <title>Users</title>
+    <style>
+        * {
+            margin:0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        .add_user {
+            display:inline-block;
+            position: relative;
+            left: 50px;
+        }
+        .users-table {
+            display:inline-block;
+            position: relative;
+            top: 70px;
+            left: 50px;
+        }
+    </style>
 </head>
 <body>
     <div class="container-fluid">
-        
-            <form class="insert_form col-lg-6 col-md-6 col-sm-12 col-xs-12" action="insert_user.php" method="POST" >
+        <div class="row">
+             <form class="insert_form col-lg-6 col-md-6 col-sm-12 col-xs-12" action="insert_user.php" method="POST" >
                <label for="username"> Enter username
                 <input type="text" class="username" name="username">
                </lablel>
-               <label for="password"></label> Enter Passsword
+               <label for="password">
+                <input type="password"class="userpassword" name="userpassword" placeholder="Enter user password">
+
+               </label> 
+
                <button class="add_user btn btn-success">Add new user</button>
+
 
                <table class="users-table table table-light ">
                 <thead>
@@ -37,6 +59,9 @@
                </table>
 
             </form>
+
+        </div>
+           
 
     </div>
     
